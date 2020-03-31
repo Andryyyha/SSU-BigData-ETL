688100d6b3af1106721f013b459131574f075966
Yana Preobrazhenskaya <yanak.preobrazhenskaya@yandex.ru>
2020-04-01 01:51:14 +0400

    feat: stream reading

--- ssuBigData/src/main/scala/Main.scala
+++ ssuBigData/src/main/scala/Main.scala
@@ -1,96 +1,96 @@
-import com.amazonaws.services.kinesis.AmazonKinesisClientBuilder
-import com.amazonaws.services.kinesis.model.{GetRecordsRequest, GetShardIteratorRequest, ShardIteratorType}
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
+import com.amazonaws.services.kinesis.AmazonKinesisClient
+import com.amazonaws.services.kinesis.clientlibrary.lib.worker.InitialPositionInStream
 import com.typesafe.config.ConfigFactory
+import org.apache.spark.rdd.RDD
 import org.apache.spark.SparkConf
-import org.apache.spark.streaming.{Milliseconds, StreamingContext}
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.types.{DoubleType, IntegerType, StringType, StructField, StructType}
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.kinesis.KinesisInputDStream
+import org.apache.spark.streaming.{Milliseconds, Seconds, StreamingContext}
 
 object Main {
 
-  def main(args: Array[String]): Unit = {
+  val schema = StructType(
+    List(
+      StructField("date", StringType, nullable = true),
+      StructField("BEN", DoubleType, nullable = true),
+      StructField("CO", DoubleType, nullable = true),
+      StructField("EBE", DoubleType, nullable = true),
+      StructField("MXY", DoubleType, nullable = true),
+      StructField("NMHC", DoubleType, nullable = true),
+      StructField("NO_2", DoubleType, nullable = true),
+      StructField("NOx", DoubleType, nullable = true),
+      StructField("OXY", DoubleType, nullable = true),
+      StructField("O_3", DoubleType, nullable = true),
+      StructField("PM10", DoubleType, nullable = true),
+      StructField("PXY", DoubleType, nullable = true),
+      StructField("SO_2", DoubleType, nullable = true),
+      StructField("TCH", DoubleType, nullable = true),
+      StructField("TOL", DoubleType, nullable = true),
+      StructField("station", IntegerType, nullable = true)
+    )
+  )
 
+  def extractAir(input: RDD[Array[Byte]]) = {
+    input.map(w => Row.fromSeq(new String(w).split(",")))
+//    input
+//      .filter(_.length != 0)
+//      .map(attribute => Row.fromSeq(attribute))
+  }
+
+  def main(args: Array[String]) : Unit = {
     val batchInterval = Milliseconds(2000)
 
     val kinesisConf = ConfigFactory.parseResources("application.conf")
-    
+
     val appName = kinesisConf.getString("conf.appName")
     val streamName = kinesisConf.getString("conf.streamNameAirChecks")
     val endpointUrl = kinesisConf.getString("conf.endpointUrl")
     val region = kinesisConf.getString("conf.region")
 
-    val conf = new SparkConf().setAppName(appName)
-    conf.setIfMissing("spark.master", "local[*]")
-
-    val kinesisClient = AmazonKinesisClientBuilder.defaultClient()
-
+    val conf = new SparkConf().setMaster("local[2]").setAppName(appName)
     val ssc = new StreamingContext(conf, batchInterval)
-    val shard = kinesisClient.describeStream(streamName).getStreamDescription.getShards.get(0)
 
-    val shardIterator = kinesisClient
-      .getShardIterator(
-        new GetShardIteratorRequest()
-          .withStreamName(streamName)
-          .withShardId(shard.getShardId)
-          .withStartingSequenceNumber(shard.getSequenceNumberRange.getStartingSequenceNumber)
-          .withShardIteratorType(ShardIteratorType.AT_SEQUENCE_NUMBER)
-      ).getShardIterator
+    val credentials = new DefaultAWSCredentialsProviderChain().getCredentials()
+    val kinesisClient = new AmazonKinesisClient(credentials)
+    kinesisClient.setEndpoint(endpointUrl)
+    val numShards = kinesisClient.describeStream(streamName).getStreamDescription().getShards().size
+    val numStreams = numShards
 
-    println(shardIterator)
-
-    val getRecordsRequest = new GetRecordsRequest
-
-    getRecordsRequest.setShardIterator(shardIterator)
-    getRecordsRequest.setLimit(25)
-
-    val recordsResult = kinesisClient.getRecords(getRecordsRequest)
-    val records = recordsResult.getRecords
-
-    records.forEach(record => println(new String(record.getData.array())))
-
-
-//    val stream = KinesisInputDStream
-//      .builder
-//      .streamingContext(ssc)
-//      .checkpointAppName(appName)
-//      .streamName(streamName)
-//      .initialPositionInStream(InitialPositionInStream.LATEST)
-//      .endpointUrl(endpointUrl)
-//      .regionName(region)
-//      .checkpointInterval(batchInterval)
-//      .storageLevel(StorageLevel.MEMORY_AND_DISK_2)
-//      .build()
-//
-//    val airCheckData = stream.map { byteArray =>
-//      val Array(r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16) = new String(byteArray).split(",")
-//      AirCheck(r1, r2,  Utils.toDouble(r3),  Utils.toDouble(r4),  Utils.toDouble(r5),  Utils.toDouble(r6),
-//        Utils.toDouble(r7),  Utils.toDouble(r8), Utils.toDouble(r9),  Utils.toDouble(r10), Utils.toDouble(r11),
-//        Utils.toDouble(r12),  Utils.toDouble(r13),  Utils.toDouble(r14), Utils.toDouble(r15), Utils.toInt(r16))
-//    }
-//
-//    val coPollution = airCheckData.filter(_.CO.get > 0)
-//
-//    coPollution.print(1)
-//
-//    println(s"dates with CO pollution > 0")
-//    coPollution.map { data =>
-//      println(s"Date is ${data.date} on station ${data.station}")
-//    }
-//
-//    // CO pollution checks over the last 20 seconds
-//    coPollution.window(Seconds(20)).foreachRDD { rdd =>
-//      val spark = SparkSession.builder.config(rdd.sparkContext.getConf).getOrCreate()
-//      import spark.implicits._
-//      val hotSensorDF = rdd.toDF()
-////      hotSensorDF.createOrReplaceTempView("air_checks")
-//      hotSensorDF.show()
+//    val kinesisStreams = (0 until numStreams).map { i =>
+      val kinesisStreams =  KinesisInputDStream.builder
+        .streamingContext(ssc)
+        .endpointUrl(endpointUrl)
+        .regionName(region)
+        .streamName(streamName)
+        .checkpointAppName(appName)
+        .storageLevel(StorageLevel.MEMORY_AND_DISK_2)
+        .build()
 //    }
 
-//    ssc.remember(Minutes(1))
-//    ssc.start()
-//    ssc.awaitTerminationOrTimeout(2000 * 10 )
+//    val unionStreams = ssc.union(kinesisStreams)
+
+    val spark: SparkSession = SparkSession
+      .builder()
+      .appName(appName)
+      .config(conf)
+      .getOrCreate()
+
+//    val Seq(projectID, windowInterval, slidingInterval, totalRunningTime) = args.toSeq
+
+    kinesisStreams.foreachRDD {
+        rdd =>
+          import spark.implicits._
+//          val crashDF = rdd.toDF()
+          val crashDF = spark.createDataFrame(extractAir(rdd), schema)
+            .cache()
+
+          crashDF.show()
+      }
+
+    ssc.start()
+    ssc.awaitTerminationOrTimeout(2000 * 10 )
   }
-
 }
-
-
-
-
