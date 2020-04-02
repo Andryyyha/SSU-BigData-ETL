15acc45f6f22ff827deb5babff3acfbd545a5e81
Yana Preobrazhenskaya <yanak.preobrazhenskaya@yandex.ru>
2020-04-02 16:25:09 +0400

    feat: metrics

--- ssuBigData/src/main/scala/Main.scala
+++ ssuBigData/src/main/scala/Main.scala
@@ -1,96 +1,76 @@
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
-import com.amazonaws.services.kinesis.AmazonKinesisClient
-import com.amazonaws.services.kinesis.clientlibrary.lib.worker.InitialPositionInStream
-import com.typesafe.config.ConfigFactory
-import org.apache.spark.rdd.RDD
-import org.apache.spark.SparkConf
-import org.apache.spark.sql.{Row, SparkSession}
-import org.apache.spark.sql.types.{DoubleType, IntegerType, StringType, StructField, StructType}
-import org.apache.spark.storage.StorageLevel
-import org.apache.spark.streaming.kinesis.KinesisInputDStream
-import org.apache.spark.streaming.{Milliseconds, Seconds, StreamingContext}
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 
 object Main {
 
-  val schema = StructType(
+  val elevationCol = "elevation"
+
+  val stationsSchema = StructType(
     List(
-      StructField("date", StringType, nullable = true),
-      StructField("BEN", DoubleType, nullable = true),
-      StructField("CO", DoubleType, nullable = true),
-      StructField("EBE", DoubleType, nullable = true),
-      StructField("MXY", DoubleType, nullable = true),
-      StructField("NMHC", DoubleType, nullable = true),
-      StructField("NO_2", DoubleType, nullable = true),
-      StructField("NOx", DoubleType, nullable = true),
-      StructField("OXY", DoubleType, nullable = true),
-      StructField("O_3", DoubleType, nullable = true),
-      StructField("PM10", DoubleType, nullable = true),
-      StructField("PXY", DoubleType, nullable = true),
-      StructField("SO_2", DoubleType, nullable = true),
-      StructField("TCH", DoubleType, nullable = true),
-      StructField("TOL", DoubleType, nullable = true),
-      StructField("station", IntegerType, nullable = true)
+      StructField("id", StringType, nullable = true),
+      StructField("name", StringType, nullable = true),
+      StructField("address", StringType, nullable = true),
+      StructField("lon", StringType, nullable = true),
+      StructField("lat", StringType, nullable = true),
+      StructField("elevation", IntegerType, nullable = true)
     )
   )
 
-  def extractAir(input: RDD[Array[Byte]]) = {
-    input.map(w => Row.fromSeq(new String(w).split(",")))
-//    input
-//      .filter(_.length != 0)
-//      .map(attribute => Row.fromSeq(attribute))
+  def minMaxLvl(data: DataFrame) =
+    data
+      .agg(
+        min(elevationCol),
+        max(elevationCol),
+      )
+
+  def avgLvl(data: DataFrame) =
+    data
+      .agg(
+        avg(elevationCol)
+      )
+
+  def medianLvl(data: DataFrame, spark: SparkSession) = {
+    val w = Window.orderBy("id")
+    val dataWithIndex = data.withColumn("index", row_number().over(w))
+    val count = data.count().toInt
+    val halfCount = count / 2
+    val nextHalfCount = halfCount + 1
+    var median = 0.0
+    if (count % 2 == 0) {
+      val left = dataWithIndex.where(s"index == $halfCount").select(elevationCol).collect.map(x => x.get(0)).mkString.toDouble
+      val right = dataWithIndex.where(s"index == $nextHalfCount").select(elevationCol).collect.map(x => x.get(0)).mkString.toDouble
+      median = (left + right) / 2
+    } else {
+      median = dataWithIndex.where(s"index == $halfCount").select(elevationCol).collect.map(x => x.get(0)).mkString.toDouble
+    }
+    import spark.implicits._
+    val result = Seq(median).toDF("median")
+    result
   }
 
   def main(args: Array[String]) : Unit = {
-    val batchInterval = Milliseconds(2000)
-
-    val kinesisConf = ConfigFactory.parseResources("application.conf")
-
-    val appName = kinesisConf.getString("conf.appName")
-    val streamName = kinesisConf.getString("conf.streamNameAirChecks")
-    val endpointUrl = kinesisConf.getString("conf.endpointUrl")
-    val region = kinesisConf.getString("conf.region")
-
-    val conf = new SparkConf().setMaster("local[2]").setAppName(appName)
-    val ssc = new StreamingContext(conf, batchInterval)
-
-    val credentials = new DefaultAWSCredentialsProviderChain().getCredentials()
-    val kinesisClient = new AmazonKinesisClient(credentials)
-    kinesisClient.setEndpoint(endpointUrl)
-    val numShards = kinesisClient.describeStream(streamName).getStreamDescription().getShards().size
-    val numStreams = numShards
-
-//    val kinesisStreams = (0 until numStreams).map { i =>
-      val kinesisStreams =  KinesisInputDStream.builder
-        .streamingContext(ssc)
-        .endpointUrl(endpointUrl)
-        .regionName(region)
-        .streamName(streamName)
-        .checkpointAppName(appName)
-        .storageLevel(StorageLevel.MEMORY_AND_DISK_2)
-        .build()
-//    }
-
-//    val unionStreams = ssc.union(kinesisStreams)
-
     val spark: SparkSession = SparkSession
       .builder()
-      .appName(appName)
-      .config(conf)
+      .appName("testApp")
+      .config("spark.master", "local[2]")
       .getOrCreate()
 
-//    val Seq(projectID, windowInterval, slidingInterval, totalRunningTime) = args.toSeq
+    val airDF = spark.read
+      .format("csv")
+      .option("header", "true")
+      .option("mode", "DROPMALFORMED")
+      .load("test-air.csv")
 
-    kinesisStreams.foreachRDD {
-        rdd =>
-          import spark.implicits._
-//          val crashDF = rdd.toDF()
-          val crashDF = spark.createDataFrame(extractAir(rdd), schema)
-            .cache()
+    val stationsDF = spark.read
+      .format("csv")
+      .option("header", "true")
+      .schema(stationsSchema)
+      .load("test-stations.csv")
 
-          crashDF.show()
-      }
-
-    ssc.start()
-    ssc.awaitTerminationOrTimeout(2000 * 10 )
+    minMaxLvl(stationsDF).show()
+    avgLvl(stationsDF).show()
+    medianLvl(stationsDF, spark).show()
   }
 }
