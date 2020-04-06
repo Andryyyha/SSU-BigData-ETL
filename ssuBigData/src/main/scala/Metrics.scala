561b13da62b626a925032f1077f44eed58b81e37
miffn3 <miffn3@yandex.ru>
2020-04-06 23:13:32 +0400

    fix: missing dot

--- ssuBigData/src/main/scala/Metrics.scala
+++ ssuBigData/src/main/scala/Metrics.scala
@@ -23,7 +23,7 @@ object Metrics {
   def avgLvl(data: DataFrame): DataFrame =
     data
       .agg(
-        avg(elevationCol)alias("avg_elevation")
+        avg(elevationCol).alias("avg_elevation")
       )
 
   def medianLvl(data: DataFrame, spark: SparkSession): DataFrame = {
