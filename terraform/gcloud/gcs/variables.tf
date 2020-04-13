ecab1a94d4573d7054fe4c9f5def870d6e4b2092
Andrey Vyazkov <adrianvalir@gmail.com>
2020-04-13 14:44:40 +0400

    Create tf scripts for infra, create data generator for pub/sub

---
+++ terraform/gcloud/gcs/variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  type    = string
+  default = "ssu-bigdata-etl"
+}
+
+variable "path_to_account_key" {
+  type = string
+  default = "/Users/andryyyha/SSU-BigData-ETL-3b758a590d47.json"
+}
---
+++ terraform/gcloud/pubsub/variables.tf
@@ -0,0 +1,20 @@
+variable "air" {
+  type    = string
+  default = "air_pollution"
+}
+
+variable "project_id" {
+  type    = string
+  default = "ssu-bigdata-etl"
+}
+
+variable "path_to_account_key" {
+  type = string
+  default = "/Users/andryyyha/SSU-BigData-ETL-3b758a590d47.json"
+}
+
+variable "stations" {
+  type = string
+  default = "stations"
+}
+
